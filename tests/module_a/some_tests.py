e352b7b759e015b603e936f0850d6eee89fe6df1
martijho <martinhovin91@gmail.com>
2025-03-15 18:23:52 +0100

    .

--- tests/module_a/some_tests.py
+++ tests/module_a/some_tests.py
@@ -1,10 +1,12 @@
 
-
+# Placeholder nr 1
 def test_nr_one(): 
     pass 
 
+# Placeholder nr 2
 def test_nr_two(): 
     pass
 
+# Placeholder nr 3
 def test_nr_three(): 
     pass
--- tests/module_b/some_tests.py
+++ tests/module_b/some_tests.py
@@ -1,10 +1,11 @@
-
-# These tests are placeholders
+# Placeholder nr 1
 def test_nr_one(): 
     pass 
 
+# Placeholder nr 2
 def test_nr_two(): 
     pass
 
+# Placeholder nr 3
 def test_nr_three(): 
     pass
